b6ceb9b44b77991ad9e40a8d64597f122e9d8239
ahmed khelili <ahmedkhelili132@gmail.com>
2022-03-24 22:44:58 +0100

    get image from url

--- Controllers/imagesProcessing.ts
+++ Controllers/imagesProcessing.ts
@@ -1,4 +1,5 @@
 import sharp from "sharp";
+import axios from "axios";
 import multer from "multer";
 import fs from "fs";
 import { readFile } from "fs/promises";
@@ -48,4 +49,36 @@ export const handleImageCompression = async (
       .end();
   }
   next();
+};
+
+export const getImagesMetaData = async (req: Request, res: Response) => {
+  try {
+    console.log(req.body);
+    res.status(201).json({
+      message: "Target and Source image successfully received",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const handleUploadImageByUrl = async (req: Request, res: Response) => {
+  const { imageURL } = req.body;
+  try {
+    const response = await axios.get(
+      "https://images.pexels.com/photos/9298346/pexels-photo-9298346.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      {
+        responseType: "arraybuffer",
+      }
+    );
+
+    //convert image from base64 to a Buffer
+    // const imgBuffer = Buffer.from(response.data, "base64");
+
+    //get image metadata
+    const metadata = await sharp(response.data).metadata();
+    console.log(metadata);
+  } catch (error) {
+    console.log(error);
+  }
 };
--- Routes/imagesProcessing.ts
+++ Routes/imagesProcessing.ts
@@ -1,5 +1,9 @@
 import express from "express";
-import { handleImageCompression } from "../Controllers/imagesProcessing";
+import {
+  handleImageCompression,
+  getImagesMetaData,
+  handleUploadImageByUrl,
+} from "../Controllers/imagesProcessing";
 import multer from "multer";
 
 const storage = multer.memoryStorage();
@@ -8,5 +12,8 @@ const route = express.Router();
 
 //compression
 route.post("/compression", upload.single("file"), handleImageCompression);
+route.post("/upload-by-url", handleUploadImageByUrl);
+//color matching
+route.post("/matching", upload.array("file", 2), getImagesMetaData);
 
 export default route;
